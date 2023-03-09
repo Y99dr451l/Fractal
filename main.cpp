efd337572d1b7e9136195729806f98eba02b9efa
Y99dr451l <matthias@kockisch.eu>
2023-03-09 17:06:47 +0100

    added complex

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <thread>
 #include <mutex>
 #include <iostream>
+#include <complex>
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 
@@ -23,9 +24,10 @@ void hsv_rgb(double h, double s, double v, double& r, double& g, double& b) {
 
 int main() {
 	sf::RenderWindow window(sf::VideoMode::getDesktopMode(), "SFML window", sf::Style::Fullscreen);
+	window.setFramerateLimit(60);
 	int windowWidth = window.getSize().x, windowHeight = window.getSize().y;
 	int maxDepth = 1000;
-	double x0 = 0., y0 = 0.; // initial z
+	std::complex<double> z0(0., 0.);
 	double imageHeight = 3., imageWidth = imageHeight * windowWidth / windowHeight;
 	double imageCenterX = -0.75, imageCenterY = 0.;
 	sf::Uint8* pixels = new sf::Uint8[windowWidth * windowHeight * 4];
@@ -35,29 +37,28 @@ int main() {
 	for (int n = 0; n < nThreads; n++) threads[n] = std::thread([=, &pixels] {
 		for (int px = n; px < windowWidth; px += nThreads)
 			for (int py = 0; py < windowHeight; py++) {
-				double cx = ((double)px - 0.5 * windowWidth) / windowWidth * imageWidth + imageCenterX;
-				double cy = ((double)py - 0.5 * windowHeight) / windowHeight * imageHeight + imageCenterY;
+				std::complex<double> c(((double)px - 0.5 * windowWidth) / windowWidth * imageWidth + imageCenterX,
+									   ((double)py - 0.5 * windowHeight) / windowHeight * imageHeight + imageCenterY);
 				int exitDepth = maxDepth;
-				double xn = x0, yn = y0;
+				std::complex<double> zn = z0;
 				for (int i = 0; i < maxDepth; i++) {
-					double xtemp = xn * xn - yn * yn + cx;
-					yn = 2 * xn * yn + cy;
-					xn = xtemp;
-					if (xn * xn + yn * yn > 4) {
+					zn = std::pow(zn, 2) + c;
+					if (abs(zn) > 2) {
 						exitDepth = i;
 						break;
 					}
 				}
 				int i = px + py * windowWidth;
+				double h = powf(360 * (double)exitDepth / maxDepth, 2);
 				double r = 0, g = 0, b = 0;
-				if (exitDepth != maxDepth) hsv_rgb(360 * (double)exitDepth / maxDepth, 1., 1., r, g, b);
+				if (exitDepth != maxDepth) hsv_rgb(h, 1., 1., r, g, b);
 				pixels[4 * i] = (sf::Uint8)r;
 				pixels[4 * i + 1] = (sf::Uint8)g;
 				pixels[4 * i + 2] = (sf::Uint8)b;
 				pixels[4 * i + 3] = 255;
 			}
 		});
-	for (int n = 0; n < nThreads; n++) threads[n].join();
+	for (int n = 0; n < nThreads; n++) threads[n].detach();
 	sf::Texture texture;
 	texture.create(windowWidth, windowHeight);
 	texture.update(pixels);
@@ -67,6 +68,8 @@ int main() {
 		while (window.pollEvent(event)) {
 			if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape) window.close();
 		}
+		texture.update(pixels);
+		sf::Sprite sprite(texture);
 		window.clear();
 		window.draw(sprite);
 		window.display();
